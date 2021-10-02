a7da50328861c7c1335eb954dab6a07bf643496f
dios86L <77297933+dios86L@users.noreply.github.com>
2021-10-02 22:17:57 +0300

    Add files via upload

--- HW4/src/test/java/TriangleTest.java
+++ HW4/src/test/java/TriangleTest.java
@@ -1,20 +1,19 @@
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class TriangleTest {
 
     @Test
-    void testLength() {
-    if (a<0 || b<0 || c<0){
-        System.out.println("Сторона треугольника не может иметь отрицательную величину");
-    } else {
-        return;
-    }
+    void positiveSquare () throws  Exception {
+        double result = Triangle.triangleSquare(3,5,8);
+        Assertions.assertEquals(0,result);
     }
 
     @Test
-    void Summ(){
-        if (a+b<=c || a+c<=b || c+b<=a){
-            System.out.println("Сумма сторон треугольника не может быть меньше или равнв третьей стороне");
-        }
+    void negativeSquare () throws  Exception {
+        assertThatExceptionOfType(Exception.class).isThrownBy(()-> Triangle.triangleSquare(-2,3,4) );
     }
 }
