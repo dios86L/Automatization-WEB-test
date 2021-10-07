e857c7beeb9c3514ae78b929eed8066201feb83e
dios86L <77297933+dios86L@users.noreply.github.com>
2021-10-07 20:26:18 +0300

    Add files via upload

--- HW6/src/main/java/org/example/CRMtest.java
+++ HW6/src/main/java/org/example/CRMtest.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class CRMtest {
 
@@ -23,68 +22,62 @@ public class CRMtest {
         driver = new ChromeDriver();
         WebDriverManager.chromedriver().setup();
 
-        AtomicReference<WebDriverWait> webDriverWait = new AtomicReference<>(new WebDriverWait(driver, 10));
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
 
         driver.get(CRM_URL);
 
         login();
 
-        driver.findElement(By.xpath("//a//span[contains(., 'Проекты')]")).isSelected();
+        List<WebElement> navMenuElements = driver.findElements(By.xpath("//a//span[contains(., 'Проекты')]"));
+        WebElement expenceElement = navMenuElements.stream().filter(e -> e.getText().equals("Проекты")).findFirst().get();
 
+        Actions actions = new Actions(driver);
+        actions.moveToElement(expenceElement).build().perform();
 
+        driver.findElement(By.xpath("//span[.='Все проекты']")).click();
 
-        driver.findElement(By.xpath("//a//span[contains(., 'Все проекты')]")).click();
-
-        webDriverWait.get().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a//span[contains(., 'Все проекты')]")));
-        webDriverWait.get().until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//a//span[contains(., 'Все проекты')]"))));
-        driver.findElement(By.xpath("//a//span[contains(., 'Все проекты')]")).click();
-
-        webDriverWait.get().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='btn-group']//a[contains(., 'Создать проект')]")));
-        webDriverWait.get().until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[@class='btn-group']//a[contains(., 'Создать проект')]"))));
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='btn-group']//a[contains(., 'Создать проект')]")));
+        webDriverWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[@class='btn-group']//a[contains(., 'Создать проект')]"))));
         driver.findElement(By.xpath("//div[@class='btn-group']//a[contains(., 'Создать проект')]")).click();
 
 
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='crm_project[name]']")));
         driver.findElement(By.xpath("//input[@name='crm_project[name]']")).sendKeys("test");
 
         driver.findElement(By.xpath("//span[contains(.,'Укажите организацию')]")).click();
 
-        driver.findElement(By.xpath("//li//div[contains(.,'12323142342134')]")).click();
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[text()='12323142342134']")));
+        driver.findElement(By.xpath("//div[text()='12323142342134']")).click();
 
-        Select priority = new Select(
-                driver.findElement(By.xpath("//select[@name='crm_project[priority]']")));
-        priority.selectByVisibleText("Низкий");
+        //priority
+        select("//select[@name='crm_project[priority]']","Низкий");
 
-        Select financeSource = new Select(
-                driver.findElement(By.xpath("//select[@name='crm_project[financeSource]']")));
-        financeSource.selectByVisibleText("Из средств заказчика");
+        //financeSource
+        select("//select[@name='crm_project[financeSource]']","Из средств заказчика");
 
-        Select businessUnit = new Select(
-                driver.findElement(By.xpath("//select[@name='crm_project[businessUnit]']")));
-        businessUnit.selectByVisibleText("Research & Development");
+        //businessUnit
+        select("//select[@name='crm_project[businessUnit]']","Research & Development");
 
-        Select curator = new Select(
-                driver.findElement(By.xpath("//select[@name='crm_project[curator]']")));
-        curator.selectByVisibleText("Амелин Владимир");
+        //curator
+        select("//select[@name='crm_project[curator]']","Амелин Владимир");
 
-        Select rp = new Select(
-                driver.findElement(By.xpath("//select[@name='crm_project[rp]']")));
-        rp.selectByVisibleText("Амелин Владимир");
+        //rp
+        select("//select[@name='crm_project[rp]']","Амелин Владимир");
 
-        Select manager = new Select(
-                driver.findElement(By.xpath("//select[@name='crm_project[manager]']")));
-        manager.selectByVisibleText("Амелин Владимир");
+        //manager
+        select("//select[@name='crm_project[manager]']","Амелин Владимир");
 
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(@id,'s2id_crm_project_contactMain-uid')]")));
+        driver.findElement(By.xpath("//div[contains(@id,'s2id_crm_project_contactMain-uid')]")).click();
 
-        driver.findElement(By.xpath("//div[@id='select2-drop-mask']")).click();
-
-        driver.findElement(By.xpath("//li//div[contains(.,'1111 Ольга')]")).click();
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[text()='1111 Ольга']")));
+        driver.findElement(By.xpath("//div[text()='1111 Ольга']")).click();
 
         driver.findElement(By.xpath("//button[contains(., 'Сохранить и закрыть')]")).click();
 
         Thread.sleep(5000);
 
         driver.quit();
-
     }
 
     public static void login() {
@@ -92,5 +85,11 @@ public class CRMtest {
         element.sendKeys("Applanatest1");
         driver.findElement(By.id("prependedInput2")).sendKeys("Student2020!");
         driver.findElement(By.id("_submit")).click();
+    }
+
+    public  static void select (String a, String b) {
+        Select menuPoint = new Select(
+                driver.findElement(By.xpath(a)));
+        menuPoint.selectByVisibleText(b);
     }
 }
