19e08138e5cf8cc84df33eef01aaedcb638beec4
dios86L <77297933+dios86L@users.noreply.github.com>
2021-10-07 20:25:43 +0300

    Add files via upload

---
+++ HW6/src/test/java/org/example/TestOnCRMtest.java
@@ -0,0 +1,76 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
+public class TestOnCRMtest {
+    WebDriver driver;
+    WebDriverWait webDriverWait;
+
+    @BeforeAll
+    static void registerDriver() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUpBrowser() {
+        driver = new ChromeDriver();
+        webDriverWait = new WebDriverWait(driver, 5);
+        driver.get("https://crm.geekbrains.space/");
+        login();
+    }
+
+    @Test
+    public void testIn() throws InterruptedException {
+        Actions actions = new Actions(driver);
+        driver.get("https://crm.geekbrains.space/project/create/");
+
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='crm_project[name]']")));
+        driver.findElement(By.xpath("//input[@name='crm_project[name]']")).click();
+        driver.findElement(By.xpath("//input[@name='crm_project[name]']")).sendKeys("test");
+
+        Assertions.assertEquals("test", driver.findElement(By.xpath("//input[@name='crm_project[name]']")).getText());
+
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void name() throws InterruptedException {
+        Actions actions = new Actions(driver);
+        driver.get("https://crm.geekbrains.space/project/create/");
+
+        driver.findElement(By.xpath("//span[contains(.,'Укажите организацию')]")).click();
+
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[text()='12323142342134']")));
+        driver.findElement(By.xpath("//div[text()='12323142342134']")).click();
+
+        Assertions.assertEquals("12323142342134", driver.findElement(By.xpath("//div[text()='12323142342134']")).getText());
+
+        Thread.sleep(5000);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+
+    public void login() {
+        WebElement element = driver.findElement(By.id("prependedInput"));
+        element.sendKeys("Applanatest1");
+        driver.findElement(By.id("prependedInput2")).sendKeys("Student2020!");
+        driver.findElement(By.id("_submit")).click();
+    }
+}
